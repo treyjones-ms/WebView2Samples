2c95cfe915ce6a50ebd9d4521a75b4ba18436cb8
Trey Jones <treyjones@microsoft.com>
2023-09-19 11:55:08 -0500

    Setup repro demo for keyboard input problem in WebView2.

--- GettingStartedGuides/WinUI2_GettingStarted/MyUWPGetStartApp/MainPage.xaml.cs
+++ GettingStartedGuides/WinUI2_GettingStarted/MyUWPGetStartApp/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,21 @@ namespace MyUWPGetStartApp
         public MainPage()
         {
             this.InitializeComponent();
+            this.InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            await this.WebView2.EnsureCoreWebView2Async();
+
+            string root = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+            string path = root + @"\Html";
+
+            // Get the folder object that corresponds to this absolute path in the file system.
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(path);
+            this.WebView2.CoreWebView2.SetVirtualHostNameToFolderMapping("demo", folder.Path, CoreWebView2HostResourceAccessKind.DenyCors);
+            this.WebView2.CoreWebView2.Navigate("https://demo/index.html");
+            this.WebView2.Focus(FocusState.Programmatic);
         }
     }
 }
